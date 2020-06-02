cec732dc326a4c4ce641e24805e0fdd0bb74eedf
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-06-02 00:53:29 -0500

    Form added to login and register

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Context from '../Context'
+import { UserForm } from '../components/UserForm'
+import { Container } from '../styles/NotRegisterUser'
 
 export const NotRegisterUser = () => (
   <Context.Consumer>
     {/* activateAuth se obtiene del Context.js de la const value y se inyecta para activar el isAuth sin necesidad de usar las props */
       ({ activateAuth }) => {
         return (
-          <form onSubmit={activateAuth}>
-            <button>Iniciar sesión</button>
-          </form>
+          <>
+            <Container>
+              <UserForm onSubmit={activateAuth} title='Registrarse' />
+              <UserForm onSubmit={activateAuth} title='Iniciar Sesión' />
+            </Container>
+          </>
         )
       }
     }
---
+++ src/styles/NotRegisterUser.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 90%;
+  height: 600px;
+  border: 1px solid rgb(219, 219,219);
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 20px;
+  margin: 90px 5%;
+`
