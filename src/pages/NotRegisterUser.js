c56f182b1fac9f99eeba423f7fec2d7175cb0b80
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-06-04 13:57:16 -0500

    Helmet added as a Layout

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -4,6 +4,7 @@ import { UserForm } from '../components/UserForm'
 import { Container } from '../styles/NotRegisterUser'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
+import { Layout } from '../components/Layout'
 
 export const NotRegisterUser = () => {
   const { activateAuth } = useContext(Context) /* activateAuth se obtiene del Context.js de la const value y se inyecta para activar el isAuth sin necesidad de usar las props */
@@ -11,44 +12,45 @@ export const NotRegisterUser = () => {
   return (
     <>
       <Container>
-        <RegisterMutation>
-          {
-            (register, { loading, error }) => {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
-                register({ variables }).then(({ data }) => {
-                  const { signup } = data
-                  activateAuth(signup)
-                })
+        <Layout title='Login' subtitle='Enter your user and password'>
+          <RegisterMutation>
+            {
+              (register, { loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  register({ variables }).then(({ data }) => {
+                    const { signup } = data
+                    activateAuth(signup)
+                  })
+                }
+
+                const errorMsg = error && 'User already registered'
+
+                return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Register' />
               }
-
-              const errorMsg = error && 'User already registered'
-
-              return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Register' />
             }
-          }
-        </RegisterMutation>
+          </RegisterMutation>
 
-        <LoginMutation>
-          {
-            (login, { error, loading }) => {
-              const onSubmit = ({ email, password }) => {
-                const input = { email, password }
-                const variables = { input }
-                login({ variables }).then(({ data }) => {
-                  const { login } = data
-                  activateAuth(login)
-                })
+          <LoginMutation>
+            {
+              (login, { error, loading }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  login({ variables }).then(({ data }) => {
+                    const { login } = data
+                    activateAuth(login)
+                  })
+                }
+
+                const errorMsg = error && 'Wrong password or user does not exist'
+
+                return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Login' />
               }
-
-              const errorMsg = error && 'Wrong password or user does not exist'
-
-              return <UserForm disabled={loading} error={errorMsg} onSubmit={onSubmit} title='Login' />
             }
-          }
-        </LoginMutation>
-
+          </LoginMutation>
+        </Layout>
       </Container>
     </>
   )
