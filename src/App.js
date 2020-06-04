daefa58ac3a87c625464b706cbd67d1830ec9623
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-06-04 13:10:44 -0500

    Secured url used

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { NavBar } from './components/NavBar'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisterUser } from './pages/NotRegisterUser'
-import Context from './Context'
+import { Context } from './Context'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {/* Tenemos un children con una función que recibe por parámetro si el usuario está autentificado */
-          ({ isAuth }) =>
-            isAuth
-              ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-              )
-              : (
-                <Router>
-                  <NotRegisterUser path='/favs' />
-                  <NotRegisterUser path='/user' />
-                </Router>
-              )
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
