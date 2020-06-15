3ad8afd5df0c89e29434754c1dd8607efbc3f9c9
Chuy <chuy@Chuys-MacBook-Pro.local>
2020-06-15 15:45:32 -0500

    Cypress added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+const path = require('path')
 
 module.exports = {
   output: {
@@ -13,6 +17,37 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Your pet photos app',
+      shortname: 'Petgram 🐶',
+      description: 'With Petgram, you can find photos of pets very easyly',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          size: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-iko-8mw0avk5b.now.sh/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
